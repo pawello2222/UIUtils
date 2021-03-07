09f51474061e9f1f3119d9a37ff20f272203a13d
pawello2222 <pawello2222@gmail.com>
2021-03-07 14:28:37 +0100

    Updated BackgroundNavigationLink

--- Sources/UIUtils/BackgroundNavigationLink.swift
+++ Sources/UIUtils/BackgroundNavigationLink.swift
@@ -8,24 +8,48 @@
 
 import SwiftUI
 
+/// Creates a hidden NavigationLink and optionally executes an action
 @available(iOS 13.0, *)
 public struct BackgroundNavigationLink<Destination, Content>: View where Destination: View, Content: View {
     @State private var linkActive = false
 
     private let destination: () -> Destination
+    private let action: (() -> Void)?
     private let content: () -> Content
 
-    public init(destination: @escaping () -> Destination, content: @escaping () -> Content) {
+    public init(
+        destination: @escaping () -> Destination,
+        action: (() -> Void)? = nil,
+        content: @escaping () -> Content
+    ) {
         self.destination = destination
+        self.action = action
         self.content = content
     }
 
-    public var body: some View {
-        Button(action: action, label: content)
-            .background(NavigationLink("", destination: destination(), isActive: $linkActive))
+    public init(
+        destination: @autoclosure @escaping () -> Destination,
+        action: (() -> Void)? = nil,
+        content: @escaping () -> Content
+    ) {
+        self.init(destination: destination, action: action, content: content)
     }
 
-    private func action() {
+    public init(
+        destination: @autoclosure @escaping () -> Destination,
+        action: (() -> Void)? = nil,
+        content: @autoclosure @escaping () -> Content
+    ) {
+        self.init(destination: destination, action: action, content: content)
+    }
+
+    public var body: some View {
+        Button(action: buttonAction, label: content)
+            .background(NavigationLink("", destination: destination(), isActive: $linkActive).hidden())
+    }
+
+    private func buttonAction() {
+        action?()
         linkActive = true
     }
 }
