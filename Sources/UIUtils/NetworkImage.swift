6f4b7d75f202b4223c229a9f68a1fe9f0bc319a0
pawello2222 <pawello2222@gmail.com>
2021-03-07 14:55:49 +0100

    Updated NetworkImage

--- Sources/UIUtils/NetworkImage.swift
+++ Sources/UIUtils/NetworkImage.swift
@@ -10,13 +10,40 @@ import Combine
 import SwiftUI
 
 @available(iOS 14.0, *)
-public struct NetworkImage: View {
-    @StateObject private var viewModel = ViewModel()
+public struct NetworkImage<
+    LoadingContent,
+    ErrorContent
+>: View where LoadingContent: View, ErrorContent: View {
+    @StateObject private var viewModel = NetworkImageViewModel()
 
     private let url: URL?
+    private let loadingContent: () -> LoadingContent
+    private let errorContent: () -> ErrorContent
 
-    public init(url: URL?) {
+    public init(
+        url: URL?,
+        loadingContent: @escaping () -> LoadingContent,
+        errorContent: @escaping () -> ErrorContent
+    ) {
         self.url = url
+        self.loadingContent = loadingContent
+        self.errorContent = errorContent
+    }
+
+    public init(
+        url: URL?,
+        loadingContent: @autoclosure @escaping () -> LoadingContent,
+        errorContent: @autoclosure @escaping () -> ErrorContent
+    ) {
+        self.url = url
+        self.loadingContent = loadingContent
+        self.errorContent = errorContent
+    }
+
+    public init(url: URL?) where LoadingContent == ProgressView<EmptyView, EmptyView>, ErrorContent == Image {
+        self.url = url
+        self.loadingContent = { ProgressView() }
+        self.errorContent = { Image(systemName: "photo") }
     }
 
     public var body: some View {
@@ -25,9 +52,9 @@ public struct NetworkImage: View {
                 Image(uiImage: uiImage)
                     .resizable()
             } else if viewModel.isLoading {
-                ProgressView()
+                loadingContent()
             } else {
-                Image(systemName: "photo")
+                errorContent()
             }
         }
         .onAppear {
@@ -37,38 +64,36 @@ public struct NetworkImage: View {
 }
 
 @available(iOS 14.0, *)
-public extension NetworkImage {
-    class ViewModel: ObservableObject {
-        @Published public var imageData: Data?
-        @Published public var isLoading = false
+public class NetworkImageViewModel: ObservableObject {
+    @Published public var imageData: Data?
+    @Published public var isLoading = false
 
-        private static let cache = NSCache<NSURL, NSData>()
+    private static let cache = NSCache<NSURL, NSData>()
 
-        private var cancellables = Set<AnyCancellable>()
+    private var cancellables = Set<AnyCancellable>()
 
-        public func loadImage(from url: URL?) {
-            isLoading = true
-            guard let url = url else {
-                isLoading = false
-                return
-            }
-            if let data = Self.cache.object(forKey: url as NSURL) {
-                imageData = data as Data
-                isLoading = false
-                return
-            }
-            URLSession.shared.dataTaskPublisher(for: url)
-                .map { $0.data }
-                .replaceError(with: nil)
-                .receive(on: DispatchQueue.main)
-                .sink { [weak self] in
-                    if let data = $0 {
-                        Self.cache.setObject(data as NSData, forKey: url as NSURL)
-                        self?.imageData = data
-                    }
-                    self?.isLoading = false
-                }
-                .store(in: &cancellables)
+    public func loadImage(from url: URL?) {
+        isLoading = true
+        guard let url = url else {
+            isLoading = false
+            return
         }
+        if let data = Self.cache.object(forKey: url as NSURL) {
+            imageData = data as Data
+            isLoading = false
+            return
+        }
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in
+                if let data = $0 {
+                    Self.cache.setObject(data as NSData, forKey: url as NSURL)
+                    self?.imageData = data
+                }
+                self?.isLoading = false
+            }
+            .store(in: &cancellables)
     }
 }
